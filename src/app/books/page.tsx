63ad516b03f171d567fc29c4826cbfb20764d938
Maverick <maverickseneris@gmail.com>
2026-09-10 20:50:29 +0800

    feat: add simple styling

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -3,11 +3,11 @@ import { books } from "@/data/book";
 export default function BooksPage() {
   return (
   <main>
-    <h1>Books</h1>
+    <h1 className="font-bold text-4xl mb-4">Books</h1>
     <ul>
       {books.map((book) => (
-        <li key={book.id}>
-          <h2>{book.title}</h2>
+        <li key={book.id} className="mb-4">
+          <h2 className="font-bold mb-1">{book.title}</h2>
           <p>{book.author}</p>
           <p>{book.status}</p>
         </li>
