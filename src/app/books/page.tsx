eaf258c95852dc7b7c6e20fb27246dde170fbadf
Maverick <maverickseneris@gmail.com>
2026-09-10 21:20:21 +0800

    feat: better styling

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,13 +5,13 @@ export default function BooksPage() {
   <main>
     <h1 className="font-bold text-4xl mb-4">Books</h1>
     <ul>
-      {books.map((book) => (
-        <li key={book.id} className="mb-4">
-          <h2 className="font-bold mb-1">{book.title}</h2>
-          <p>{book.author}</p>
-          <p>{book.status}</p>
-        </li>
-      ))}
+        {books.map((book) => (
+          <li key={book.id} className="mb-4">
+            <p className="text-2xl font-bold">{book.title}</p>
+            <p>{book.author}</p>
+            <p>{book.status}</p>
+          </li>
+        ))}
     </ul>
   </main>
   )
